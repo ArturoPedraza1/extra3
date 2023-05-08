468d084ff9c136febd2d645c351d67c0f2f0675a
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2023-05-07 20:06:41 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import ico.Ventana;
+import ico.VentanaGrid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layout");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana layout cuadricula");
+    }
+}
